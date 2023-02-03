d4b13253d5a9e4841448c4bd498952c37a34d690
Luiza <64799018+LuizaAlanis@users.noreply.github.com>
2023-02-02 23:58:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-def lenghtIsValid(strParam)
+def lenghtIsValid(strParam):
   lenght = len(strParam)
 
   least = 4
   most = 25
 
-  if least <= lenght <= most
+  if least <= lenght <= most:
     return True
 
-def isValidCharacters(strParam)
-  if strParam.isalnum() or "_" in strParam
 
+def isValidCharacters(strParam):
+  if strParam.isalnum() or "_" in strParam:
+    return True
 
 def CodelandUsernameValidation(strParam):
-  if lenghtIsValid(strParam) and strParam[0].isalpha() and isValidCharacters(strParam)
+  if lenghtIsValid(strParam) and strParam[0].isalpha() and isValidCharacters(strParam) and strParam[len(strParam) - 1] != "_":
     return True
-  else
+  else:
     return False
 
 # keep this function call here 
